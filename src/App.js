b139ae21ecfd813f6605d8c8c107ea50143f85d6
rohit236c <rohit236c@gmail.com>
2019-05-27 21:51:17 +0530

    all components working fine
    Signed-off-by: rohit236c <rohit236c@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
 class App extends React.Component{
 
  render(){
      return (
-         <div>hey there</div>
+         
+         <div>
+             hey there
+            <TodoInput />
+            <TodoList />
+         </div>
+         
      )
  }
 
---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoInput';
+export default class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                 TodoList
+                 <TodoItem />
+            </div>
+        )
+    }
+}
